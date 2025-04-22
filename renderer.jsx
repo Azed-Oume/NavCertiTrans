245ace4d200655e423d935eb089f687c0d8dccf8
Azed-Oume <oumessaoud@hotmail.fr>
2025-04-22 16:37:06 +0200

    mis e a jour et suppression du header et footer

--- renderer.jsx
+++ renderer.jsx
@@ -3,12 +3,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router-dom"; // alternative à BrowserRouter
-import { CookiesProvider } from "react-cookie";
 
 // 🧩 Importation des composants
 import App from "./src/app.jsx";
-import Footer from "./src/components/Footer.jsx";
-import CookieConsentComponent from "./src/components/CookieConsentComponent.jsx";
 
 // 🎨 Feuille de style globale
 import "./src/styles/style.css" // adapte selon ton arborescence
@@ -20,16 +17,12 @@ const root = createRoot(container);
 // 🚀 Affichage de l’interface React
 root.render(
   <HashRouter>
-    <CookiesProvider>
       {/* <div className="d-flex flex-column min-vh-100"> */}
       <div className="d-flex flex-column min-vh-100">
         <main className="flex-grow-1">
           <App />
           
         </main>
-        <Footer />
-        <CookieConsentComponent />
       </div>
-    </CookiesProvider>
   </HashRouter>
 );
